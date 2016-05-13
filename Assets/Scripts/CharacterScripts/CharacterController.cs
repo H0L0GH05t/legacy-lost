0ba950c44116a1359ae34c3130c462737289bdaf
Carl Milazzo <cxm7805@rit.edu>
2016-05-13 19:16:21 -0400

    Added rotation while on ropes for a less rigid experience and cleaned up the character controller

--- Assets/Scripts/CharacterScripts/CharacterController.cs
+++ Assets/Scripts/CharacterScripts/CharacterController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(BoxCollider2D))]
 public class CharacterController : MonoBehaviour {
 	//used in lateral movement
 	[Header("Player Variables")]
@@ -42,26 +44,13 @@ public class CharacterController : MonoBehaviour {
 	void Update()
 	{
 		//jump
-		if(ground && (Input.GetAxis("Jump") > 0 || Input.GetAxis("Vertical") > 0))
-		{
-			Jump(jumpForce);
-		}
-
-		if(rope && Input.GetAxis("Fire1") > 0)
-		{
-			GrabRope();
-		}
-
+		if(ground && (Input.GetAxis("Jump") > 0 || Input.GetAxis("Vertical") > 0)) { Jump(jumpForce); }
+		//grab rope
+		if(rope && Input.GetAxis("Fire1") > 0) { GrabRope(); }
 		//release rope
-		if(ropeHold.enabled && Input.GetAxis("Jump") > 0)
-		{
-			DropRope();
-		}
-
-		if(ropeHold.enabled && ropeHold.connectedBody != null)
-		{
-			ClimbRope();
-		}
+		if(ropeHold.enabled && Input.GetAxis("Jump") > 0) { DropRope(); }
+		//climb rope
+		if(ropeHold.enabled && ropeHold.connectedBody != null) { ClimbRope(); }
 	}
 
 	void FixedUpdate () 
@@ -72,36 +61,28 @@ public class CharacterController : MonoBehaviour {
 		anim.SetBool("Ground",ground);
 		anim.SetFloat("VerticalSpeed", character.velocity.y);
 
+		//release rope if our connected body broke
+		if(ropeHold.connectedBody == null) { DropRope(); }
+
 		//check for climbing
 		rope = Physics2D.OverlapCircle(ropeCheck.position,ropeRadius, ropeLayer);
 		float upMovement = Input.GetAxis("Vertical");
 		anim.SetFloat("ClimbDirection",upMovement);
-		if(ropeHold.enabled && ropeHold.connectedBody != null)
-		{
-			anim.SetBool("HoldingRope",true);
-		}
-		else
-		{
-			anim.SetBool("HoldingRope",false);
-		}
+		if(ropeHold.enabled && ropeHold.enabled) { anim.SetBool("HoldingRope",true); }
+		else { anim.SetBool("HoldingRope",false); }
 
 		//check for movement
 		float move = Input.GetAxis("Horizontal");
 		anim.SetFloat("Speed",Mathf.Abs(move));
-		if(!ropeHold.enabled || ropeHold.connectedBody == null)
+		if(!ropeHold.enabled)
 		{
 			//move the rigidbody
 			character.velocity = new Vector2(move * maxSpeed,character.velocity.y);
-	
+			//change the direction if need to
 			if (move > 0 && !right) { Flip(); }
 			else if(move < 0 && right) { Flip(); }
 		}
-		else
-		{
-			character.AddForce(new Vector2(move*swingForce,0),ForceMode2D.Impulse);
-		}
-
-
+		else { character.AddForce(new Vector2(move*swingForce,0),ForceMode2D.Impulse); }
 	}
 
 	void Flip()
@@ -126,6 +107,7 @@ public class CharacterController : MonoBehaviour {
 			GameObject ropeSegment = collider.gameObject;
 			ropeHold.connectedBody = ropeSegment.GetComponent<Rigidbody2D>();
 			ropeHold.enabled = true;
+			this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
 		}
 	}
 
@@ -133,6 +115,8 @@ public class CharacterController : MonoBehaviour {
 	{
 		ropeHold.connectedBody = null;
 		ropeHold.enabled = false;
+		this.transform.rotation = Quaternion.Euler(Vector3.zero);
+		this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
 	}
 
 	void ClimbRope()
@@ -142,43 +126,32 @@ public class CharacterController : MonoBehaviour {
 			lastRopeTime += Time.deltaTime;
 			return;
 		}
-
-		if(Input.GetAxis("Vertical") > 0)
+		if(Input.GetAxis("Vertical") != 0)
 		{
 			GameObject ropeObj = ropeHold.connectedBody.gameObject;
 			RopeSegment segment = ropeObj.GetComponent<RopeSegment>();
 			if(segment != null)
 			{
-				if(segment.up != null)
-				{
-					ropeHold.connectedBody = segment.up.GetComponent<Rigidbody2D>();
-				}
-				else
-				{
-					//launch them up slightly and drop the rope (we have reached the end)
-					DropRope();
-					Jump(jumpForce*ropeJumpMultiplier);
-				}
 				lastRopeTime = 0;
-			}
-		}
-		else if(Input.GetAxis("Vertical") < 0)
-		{
-			GameObject ropeObj = ropeHold.connectedBody.gameObject;
-			RopeSegment segment = ropeObj.GetComponent<RopeSegment>();
-			if(segment != null)
-			{
-				if(segment.down != null)
+				if(Input.GetAxis("Vertical") > 0)
 				{
-					ropeHold.connectedBody = segment.down.GetComponent<Rigidbody2D>();
+					if(segment.up != null) { ropeHold.connectedBody = segment.up.GetComponent<Rigidbody2D>(); }
+					else
+					{
+						//launch them up slightly and drop the rope (we have reached the end)
+						DropRope();
+						Jump(jumpForce*ropeJumpMultiplier);
+					}
 				}
-				else
+				else if(Input.GetAxis("Vertical") < 0)
 				{
+					if(segment.down != null) { ropeHold.connectedBody = segment.down.GetComponent<Rigidbody2D>(); }
 					//we are at the bottom, drop the rope
-					DropRope();
+					else { DropRope(); }
 				}
-				lastRopeTime = 0;
 			}
 		}
 	}
+
+
 }
