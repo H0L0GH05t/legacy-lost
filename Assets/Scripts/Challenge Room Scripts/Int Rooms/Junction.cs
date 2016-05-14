f61b3517cc6fcfdec18d6136e36136c20e76f2ca
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-05-13 20:18:50 -0600

    Very basic stuff for the int challenge rooms, just an orb moving from node to node

---
+++ Assets/Scripts/Challenge Room Scripts/Int Rooms/Junction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Junction : MonoBehaviour {
+
+    // the neighboring junctions
+    public Transform upJunction, downJunction, leftJunction, rightJunction;
+    // which direction our junction is "facing"
+    public enum DirectingDirection {up,down,left,right};
+    public DirectingDirection direction;
+
+    public Transform RedirectToNextJunction()
+    {
+        if (direction == DirectingDirection.down) { return downJunction; }
+        else if (direction == DirectingDirection.up) { return upJunction; }
+        else if (direction == DirectingDirection.right) { return rightJunction; }
+        else if(direction == DirectingDirection.left) { return leftJunction; }
+        // this is here so we always return something
+        else { return transform; }
+    }
+}
---
+++ Assets/Scripts/Challenge Room Scripts/Int Rooms/MagicOrb.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicOrb : MonoBehaviour {
+
+    // our starting junction that we should move to first
+    public Transform startingJunction;
+    // whatever junction we are moving towards
+    Transform targetJunction;
+    // speed of our magic orb
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // so we can assign our starting junction in the inspector
+        targetJunction = startingJunction;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float step = speed * Time.deltaTime;
+        // moves us towards our target junction
+        transform.position = Vector3.MoveTowards(transform.position, targetJunction.position, step);
+
+        // changes the junction we are moving to
+        if (transform.position == targetJunction.transform.position)
+        {
+            // get a new junction from the junction we just reached
+            targetJunction = targetJunction.gameObject.GetComponent<Junction>().RedirectToNextJunction();
+        }
+	}
+}
